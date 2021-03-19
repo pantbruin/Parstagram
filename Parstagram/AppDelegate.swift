8e1bb82f8825bb9bd9b621d4ba3598a46351a656
Jesse Pantoja <pantojabruin@gmail.com>
2021-03-19 16:52:48 -0700

    Implemented authentication

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "WJIg0jkXySbeY7Wo98EOXwW7Ktzr3H74BjVnB4co" // <- UPDATE
+                $0.clientKey = "x6o9IwmAniVzue4o9MOcyuJ4epclZMZO9aEzUsv6" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        
         return true
     }
 
