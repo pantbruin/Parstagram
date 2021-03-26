4820a9b0a40501c3bfa627b569b5ccacddd11642
Jesse Pantoja <pantojabruin@gmail.com>
2021-03-26 15:09:45 -0700

    Add ability for user to stay logged in

--- Parstagram/SceneDelegate.swift
+++ Parstagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +18,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // If this resolves to true, then the user is already logged in
+        if PFUser.current() != nil {
+            // Take the already logged in user to the first navigation controller
+            // name is the filename, in this case our storyboard is called Main because it's Main.storyboard
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            
+            // Instantiate a new viewcontroller. We want to instantiate the navigation controller we named as FeedNavigationController
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            
+            window?.rootViewController = feedNavigationController
+            
+
+            
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
